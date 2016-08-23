9dae2a8eaae633ba50d99b52091f02d69d590218
maxximus87 <maxxturner10@gmail.com>
2016-08-23 15:07:21 -0400

    127 Tests

--- minedmindsfunctionTEST.rb
+++ minedmindsfunctionTEST.rb
@@ -1,5 +1,6 @@
 require "minitest/autorun"
 require_relative "minedmindsfunction.rb"
+
 class TestMinedMindsFunction < Minitest::Test
 	def test_1_equals_1
 		assert_equal(1, 1)
@@ -10,5 +11,8 @@ class TestMinedMindsFunction < Minitest::Test
 	end
 	def test_2_returns_2
 		assert_equal(2, mined_minds(2))
+	end
+	def test_7_returns_7
+		assert_equal(7, mined_minds(7))
 	end
 end
