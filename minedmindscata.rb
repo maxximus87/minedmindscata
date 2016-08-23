86eb1e360e26f9bbbebc53fafcb02d6582801518
maxximus87 <maxxturner10@gmail.com>
2016-08-23 13:45:54 -0400

    all 3 variables

--- minedmindscata.rb
+++ minedmindscata.rb
@@ -1,11 +1,13 @@
 number = 1
 100.times do
-if number % 3 == 0
-puts "Mined Minds"
-elsif number % 5 == 0
-	puts "Mined Minds"
-else
-puts number
-end
-number = number + 1
+	if number % 3 == 0 && number % 5 == 0
+		puts "Mined Minds"
+	elsif number % 3 == 0
+		puts "Mined"
+	elsif number % 5 == 0
+		puts "Minds"
+	else
+		puts number
+	end
+	number = number + 1
 end
